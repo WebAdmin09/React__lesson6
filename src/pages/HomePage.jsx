431a2d0a07eac57dfcd3dcaca4a4450a24b40ce5
WebAdmin09 <husanorifjonov9@gmail.com>
2023-09-14 13:13:52 +0500

    vsdvdsv

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,10 @@
 import { Fragment } from "react"
 import Navbar from "../components/navbar/Navbar"
-import ProductForm from "../components/ProductForm/ProductForm"
+import ProductForm from "../components/productform/ProductForm"
 
 
 
 const HomePage = () => {
-
-
-
   return (
     <Fragment>
       <Navbar />
